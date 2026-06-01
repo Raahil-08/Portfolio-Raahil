ec5bb92dfed518beb37e3148fc3dc6433dff7c6c
Raahil-08 <raahildesai8@gmail.com>
2026-06-01 12:00:48 +0530

    fix hash URL smooth scrolling

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from "react";
 import { ReactLenis, useLenis } from "@/lib/lenis";
+import { useLocation } from "react-router-dom";
 
 interface LenisProps {
   children: React.ReactNode;
@@ -12,6 +13,20 @@ function SmoothScroll({ children, isInsideModal = false }: LenisProps) {
   const lenis = useLenis(({ scroll }) => {
     // called every scroll
   });
+  
+  const location = useLocation();
+
+  useEffect(() => {
+    if (lenis && location.hash) {
+      const targetElement = document.querySelector(location.hash);
+      if (targetElement) {
+        // Wait briefly to allow layout to settle
+        setTimeout(() => {
+          lenis.scrollTo(location.hash, { offset: 0, immediate: true });
+        }, 100);
+      }
+    }
+  }, [lenis, location.hash, location.pathname]);
 
   useEffect(() => {
     document.addEventListener("DOMContentLoaded", () => {
